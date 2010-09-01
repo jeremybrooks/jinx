fc2b4f06e55c73496f48d7baaea8ba2c8d192f61
Jeremy Brooks <jeremyb@whirljack.net>
2010-09-01 15:11:20 -0700

    Added url_z to the EXTRAS_ALL constant.

--- src/net/jeremybrooks/jinx/JinxConstants.java
+++ src/net/jeremybrooks/jinx/JinxConstants.java
@@ -164,7 +164,7 @@ public class JinxConstants {
     public static final String EXTRAS_ALL = "description,license,date_upload,date_taken," +
 	    "owner_name,icon_server,original_format,last_update,geo,tags," +
 	    "machine_tags,o_dims,views,media,path_alias,url_sq,url_t,url_s," +
-	    "url_m,url_o";
+	    "url_m,url_z,url_o";
 
 
 
