c3fe381d61fc4b99be703336aea4bec5ce8cdce4
jeremybrooks <jeremyb@whirljack.net>
2014-07-22 18:12:55 -0700

    added test

--- src/test/java/net/jeremybrooks/jinx/api/MachineTagsApiTest.java
+++ src/test/java/net/jeremybrooks/jinx/api/MachineTagsApiTest.java
@@ -1,7 +1,176 @@
+/*
+ * Jinx is Copyright 2010-2014 by Jeremy Brooks and Contributors
+ *
+ * Jinx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jinx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jinx.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.jeremybrooks.jinx.api;
 
+import net.jeremybrooks.jinx.Jinx;
+import net.jeremybrooks.jinx.OAuthAccessToken;
+import net.jeremybrooks.jinx.response.machinetags.Namespaces;
+import net.jeremybrooks.jinx.response.machinetags.Pairs;
+import net.jeremybrooks.jinx.response.machinetags.Predicates;
+import net.jeremybrooks.jinx.response.machinetags.Values;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 /**
+ *
  * Created by jeremyb on 7/10/14.
  */
 public class MachineTagsApiTest {
+
+    private static MachinetagsApi machinetagsApi;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        Properties p = new Properties();
+        p.load(OAuthApiTest.class.getResourceAsStream("/response/auth/secret.properties"));
+
+        String filename = p.getProperty("path.to.oauth.token");
+        assertNotNull(filename);
+
+        File file = new File(filename);
+        assertTrue(file.exists());
+
+        OAuthAccessToken oAuthAccessToken = new OAuthAccessToken();
+        oAuthAccessToken.load(new FileInputStream(file));
+
+        assertNotNull(oAuthAccessToken);
+        Jinx jinx = new Jinx(p.getProperty("flickr.key"), p.getProperty("flickr.secret"), oAuthAccessToken);
+//        jinx.setVerboseLogging(true);
+//        JinxLogger.setLogger(new StdoutLogger());
+        machinetagsApi = new MachinetagsApi(jinx);
+    }
+
+    @Test
+    public void testGetNamespaces() throws Exception {
+        Namespaces namespaces = machinetagsApi.getNamespaces(null, 10, 10, false);
+
+        assertNotNull(namespaces);
+        assertEquals("ok", namespaces.getStat());
+        assertEquals(0, namespaces.getCode());
+        assertEquals(new Integer(10), namespaces.getPerPage());
+        assertEquals(new Integer(10), namespaces.getPage());
+        assertNotNull(namespaces.getNamespaceList());
+        assertEquals(10, namespaces.getNamespaceList().size());
+
+        namespaces = machinetagsApi.getNamespaces("geo", 5, 1, true);
+        assertNotNull(namespaces);
+        assertEquals("ok", namespaces.getStat());
+        assertEquals(0, namespaces.getCode());
+        assertEquals(new Integer(5), namespaces.getPerPage());
+        assertEquals(new Integer(1), namespaces.getPage());
+        assertNotNull(namespaces.getNamespaceList());
+    }
+
+    @Test
+    public void testGetPairs() throws Exception {
+        Pairs pairs = machinetagsApi.getPairs(null, null, 10, 0, false);
+        assertNotNull(pairs);
+        assertEquals("ok", pairs.getStat());
+        assertEquals(0, pairs.getCode());
+        assertEquals(new Integer(10), pairs.getPerPage());
+        assertEquals(new Integer(1), pairs.getPage());
+        assertNotNull(pairs.getPairList());
+        assertEquals(10, pairs.getPairList().size());
+
+        pairs = machinetagsApi.getPairs("geo", null, 10, 0, true);
+        assertNotNull(pairs);
+        assertEquals("ok", pairs.getStat());
+        assertEquals(0, pairs.getCode());
+        assertEquals(new Integer(10), pairs.getPerPage());
+        assertEquals(new Integer(1), pairs.getPage());
+        assertNotNull(pairs.getPairList());
+        assertEquals(10, pairs.getPairList().size());
+
+        pairs = machinetagsApi.getPairs("geo", "airport", 10, 0, true);
+        assertNotNull(pairs);
+        assertEquals("ok", pairs.getStat());
+        assertEquals(0, pairs.getCode());
+        assertEquals(new Integer(10), pairs.getPerPage());
+        assertEquals(new Integer(1), pairs.getPage());
+        assertNotNull(pairs.getPairList());
+    }
+
+    @Test
+    public void testGetPredicates() throws Exception {
+        Predicates predicates = machinetagsApi.getPredicates(null, 10, 20, false);
+        assertNotNull(predicates);
+        assertEquals("ok", predicates.getStat());
+        assertEquals(0, predicates.getCode());
+        assertEquals(new Integer(10), predicates.getPerPage());
+        assertEquals(new Integer(20), predicates.getPage());
+        assertNotNull(predicates.getPredicateList());
+        assertEquals(10, predicates.getPredicateList().size());
+
+        predicates = machinetagsApi.getPredicates("geo", 10, 20, true);
+        assertNotNull(predicates);
+        assertEquals("ok", predicates.getStat());
+        assertEquals(0, predicates.getCode());
+        assertEquals(new Integer(10), predicates.getPerPage());
+        assertEquals(new Integer(20), predicates.getPage());
+        assertNotNull(predicates.getPredicateList());
+        assertEquals(10, predicates.getPredicateList().size());
+    }
+
+    @Test
+    public void testGetRecentValues() throws Exception {
+        long since = System.currentTimeMillis()/1000 - (1440*7);
+        Values values = machinetagsApi.getRecentValues(null, null, Long.toString(since), false);
+        assertNotNull(values);
+        assertEquals("ok", values.getStat());
+        assertEquals(0, values.getCode());
+        assertNotNull(values.getValueList());
+
+        values = machinetagsApi.getRecentValues("geo", null, null, false);
+        assertNotNull(values);
+        assertEquals("ok", values.getStat());
+        assertEquals(0, values.getCode());
+        assertNotNull(values.getValueList());
+
+        values = machinetagsApi.getRecentValues("geo", "airport", null, true);
+        assertNotNull(values);
+        assertEquals("ok", values.getStat());
+        assertEquals(0, values.getCode());
+        assertNotNull(values.getValueList());
+    }
+
+    @Test
+    public void testGetValues() throws Exception {
+        Values values = machinetagsApi.getValues("geo", "airport", 10, 1, false);
+        assertNotNull(values);
+        assertEquals("ok", values.getStat());
+        assertEquals(0, values.getCode());
+        assertEquals(new Integer(10), values.getPerPage());
+        assertEquals(new Integer(1), values.getPage());
+        assertNotNull(values.getValueList());
+
+        values = machinetagsApi.getValues("geo", "airport", 0, 0, true);
+        assertNotNull(values);
+        assertEquals("ok", values.getStat());
+        assertEquals(0, values.getCode());
+        assertNotNull(values.getValueList());
+    }
+
 }
